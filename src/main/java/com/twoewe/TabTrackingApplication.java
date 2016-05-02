b42d7a0c0393e72924f004809e3a8622e8f89216
jflo <jflorentine@gmail.com>
2016-05-02 08:20:22 -0400

    First post!

---
+++ src/main/java/com/twoewe/TabTrackingApplication.java
@@ -0,0 +1,12 @@
+package com.twoewe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabTrackingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/twoewe/TabTrackingApplicationTests.java
@@ -0,0 +1,18 @@
+package com.twoewe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TabTrackingApplication.class)
+@WebAppConfiguration
+public class TabTrackingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
