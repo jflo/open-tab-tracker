16b21a4204509ccee5c1540d569a723a2385d97f
jflo <jflorentine@gmail.com>
2016-05-02 21:57:51 -0400

    crude sketch of query

--- src/main/java/com/twoewe/model/ClueRepo.java
+++ src/main/java/com/twoewe/model/ClueRepo.java
@@ -7,4 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 @Transactional
 public interface ClueRepo extends CrudRepository<Clue, Long>{
 
+	//percentage of time per client for this week
+	//select client_name, (count(client_name) * 100 / (select count(*) from Clue)) as work from Clue where WEEK(accessed_at) = WEEK(SYSDATE())group By client_name;
 }
