a1840edc870ce6b9867b51fc0e6d0623b0f4efd6
jflo <jflorentine@gmail.com>
2016-05-03 14:59:31 -0400

    purged

--- src/main/java/com/twoewe/service/ClientMapper.java
+++
@@ -1,9 +0,0 @@
-package com.twoewe.service;
-
-import java.util.regex.Pattern;
-
-public interface ClientMapper {
-
-	public String map(String uri);
-	public void addMapping(Pattern p, String clientName);
-}
